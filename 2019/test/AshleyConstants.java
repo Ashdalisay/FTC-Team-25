2694584fa49c0c31b74e1cc5d341ec693ca97579
ashley Dalisay <ashleydalisay@ashleys-MacBook-Pro-7.local>
2019-03-26 18:38:32 -0700

    adding two wheel and four wheel drive task examples for outreach robot

---
+++ 2019/test/AshleyConstants.java
@@ -0,0 +1,10 @@
+package test;
+
+public class AshleyConstants {
+    private static double SERVO_DOMAIN = 256.0;
+    public static double RIGHT_GRAB_OPEN = 10/SERVO_DOMAIN;
+    public static double RIGHT_GRAB_CLOSED = 15/SERVO_DOMAIN;
+    public static double LEFT_GRAB_OPEN = 12/SERVO_DOMAIN;
+    public static double LEFT_GRAB_CLOSED = 17/SERVO_DOMAIN;
+
+}
